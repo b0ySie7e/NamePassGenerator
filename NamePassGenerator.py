44ccb74974fbedfe06079b98b7bd825dd0745295
b0ySie7e <zombi1532ao@gmail.com>
2024-03-01 13:07:00 -0500

    upload script

---
+++ NamePassGenerator.py
@@ -0,0 +1,102 @@
+#/usr/bin/python3
+
+import argparse
+import sys
+import os
+import signal
+from pwn import *
+
+def signal_handler(signal, frame):
+    print("\n[+] Exiting.")
+    sys.exit(0)
+
+def gen_with_file(file):
+    
+    input_file = file
+    output_file = "wordlist.txt"
+
+    if os.path.exists(output_file):
+        print("[!] {} file already exists.".format(output_file))
+        sys.exit(1)
+    
+    log.info('[🤔] Generating...')
+    with open(input_file, 'r') as file:
+        with open(output_file, 'w') as output:
+            for line in file:
+                name, lastname = line.strip().split(' ')
+                name = name.lower()
+                lastname = lastname.lower()
+                
+                formatted_names = [
+                    "{}{}".format(name, lastname),
+                    "{}-{}".format(name, lastname),
+                    "{}.{}".format(name, lastname),
+                    "{}{}".format(name[0], lastname),
+                    "{}-{}".format(name[0], lastname),
+                    "{}.{}".format(name[0], lastname),
+                    "{}{}".format(lastname, name),
+                    "{}-{}".format(lastname, name),
+                    "{}.{}".format(lastname, name),
+                    "{}{}".format(lastname, name[0]),
+                    "{}-{}".format(lastname, name[0]),
+                    "{}.{}".format(lastname, name[0]),
+                    "{}{}".format(name[:3], lastname[:3]),
+                    "{}.{}".format(name[:3], lastname[:3])
+                ]
+
+                for formatted_name in formatted_names:
+                    output.write(formatted_name + '\n')
+            log.progress('Task completed and Saved in the file %s'%output_file)
+
+def gen_with_data_user(name, lastname):
+    output_file = "wordlistUser.txt"
+    
+    if os.path.exists(output_file):
+        print("[!] {} file already exists.".format(output_file))
+        sys.exit(1)
+    
+    log.info('[🤔] Generating...')
+    with open(output_file, 'w') as output:
+        formatted_names = [
+        "{}{}".format(name, lastname),
+        "{}-{}".format(name, lastname),
+        "{}.{}".format(name, lastname),
+        "{}{}".format(name[0], lastname),
+        "{}-{}".format(name[0], lastname),
+        "{}.{}".format(name[0], lastname),
+        "{}{}".format(lastname, name),
+        "{}-{}".format(lastname, name),
+        "{}.{}".format(lastname, name),
+        "{}{}".format(lastname, name[0]),
+        "{}-{}".format(lastname, name[0]),
+        "{}.{}".format(lastname, name[0]),
+        "{}{}".format(name[:3], lastname[:3]),
+        "{}.{}".format(name[:3], lastname[:3])
+        ]
+        for formatted_name in formatted_names:
+            output.write(formatted_name + '\n')
+    log.progress('Task completed and Saved in the file %s'%output_file)
+
+if __name__=='__main__':
+    signal.signal(signal.SIGINT, signal_handler)
+    
+    parser=argparse.ArgumentParser(description='Generate a list of users from a firts and last name')
+    parser.add_argument('-w','--file', help='file containning the list of users')
+    parser.add_argument('-n', '--name', help='Name')
+    parser.add_argument('-s', '--surname', help='Surname')
+    args = parser.parse_args()
+    
+    if args.file:
+        file=args.file
+        if os.path.exists(file):
+            gen_with_file(file)
+        else:
+            print("[😒] file does not exist...")
+    elif args.name and args.surname:
+        name=args.firtsname
+        surname=args.surname
+        gen_with_data_user(name, surname)
+
+    else:
+        parser.print_help()
+    
--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # NamePassGenerator
 This script generates passwords based on the user's first name and last name.
+
+
+```shell
+❯ python3 NamePassGenerator.py
+usage: NamePassGenerator.py [-h] [-w FILE] [-n NAME] [-s SURNAME]
+
+Generate a list of users from a firts and last name
+
+options:
+  -h, --help            show this help message and exit
+  -w FILE, --file FILE  file containning the list of users
+  -n NAME, --name NAME  Name
+  -s SURNAME, --surname SURNAME
+                        Surname
+                                                             
+```
+
+```shell
+❯ cat users.txt
+b0y seven
+b0y four
+            
+```
+
+In the script you can generate two types of word lists, one using a file and the other using a first and last name
+
+# Use file
+
+```shell
+❯ python3 NamePassGenerator.py -w users.txt
+[*] [🤔] Generating...
+[←] Task completed and Saved in the file wordlist.txt
+```
+
+```shell
+❯ cat wordlist.txt
+b0yseven
+b0y-seven
+b0y.seven
+bseven
+b-seven
+b.seven
+sevenb0y
+seven-b0y
+seven.b0y
+sevenb
+seven-b
+seven.b
+b0ysev
+b0y.sev
+b0yfour
+b0y-four
+b0y.four
+bfour
+b-four
+b.four
+fourb0y
+four-b0y
+four.b0y
+fourb
+four-b
+four.b
+b0yfou
+b0y.fou
+```
+# Use a firts and last name
+
+```shell
+❯ python3 NamePassGenerator.py -n b0y -s seven
+[*] [🤔] Generating...
+[.] Task completed and Saved in the file wordlistUser.txt
+```
+
+```shell
+❯ cat wordlistUser.txt
+b0yseven
+b0y-seven
+b0y.seven
+bseven
+b-seven
+b.seven
+sevenb0y
+seven-b0y
+seven.b0y
+sevenb
+seven-b
+seven.b
+b0ysev
+b0y.sev
+
+```
